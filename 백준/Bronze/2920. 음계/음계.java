5fcdbb0eee7a0c77a5b240ba8b31aa8e3ad55791
SeonYoo-Kim <tjdn9@naver.com>
2026-02-07 23:34:04 +0900

    [Bronze II] Title: 음계, Time: 152 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String result;
+		switch (str) {
+		case "1 2 3 4 5 6 7 8": {
+			System.out.println("ascending");
+			break;
+		}case "8 7 6 5 4 3 2 1": {
+			System.out.println("descending");
+			break;
+		}
+		default:
+			System.out.println("mixed");
+		}
+
+	}
+
+}
