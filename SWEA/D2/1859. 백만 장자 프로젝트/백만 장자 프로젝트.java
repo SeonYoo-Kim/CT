559d2d534a7b0b82ffe22242497ad8526233bc98
SeonYoo-Kim <tjdn9@naver.com>
2026-02-09 06:25:07 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 370 ms, Memory: 139,168 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution{
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			int N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+//			int totalBenifit = 0;
+//			for (int i = 0; i < N - 1; i++) {
+//				int dailyMax = 0;
+//				for (int j = i + 1; j < N; j++) {
+//					int benifit = arr[j] - arr[i];
+//					if (benifit > dailyMax)
+//						dailyMax = benifit;
+//				}
+//				totalBenifit += dailyMax;
+//			}
+//			System.out.println("#" + tc + " " + totalBenifit);
+			int max = arr[N - 1];
+			long sum = 0L;
+			for (int i = N - 2; i >= 0; i--) {
+				if (arr[i] > max) max = arr[i];
+				else sum += max - arr[i];
+
+			}
+			System.out.println("#" + tc + " " + sum);
+		}
+
+	}
+
+}
