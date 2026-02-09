d22a7da43b9e9588357da51acf8881608a78d717
SeonYoo-Kim <tjdn9@naver.com>
2026-02-10 07:00:05 +0900

    [Bronze II] Title: 분해합, Time: 172 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,22 +1,21 @@
-
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String num = sc.nextLine().trim();
-		int n = Integer.valueOf(num);
-		
-		for (int i = n-(num.length()*10); i < n; i++) {
-			// System.out.println("===== i : " + i +"=====");
-			int sum = i;
-			int tmp = i;
-			for (int j = 1; j <= num.length(); j++) {
-				// System.out.println("+++++ j : " + j +"+++++ " + (tmp % 10));
-				sum += tmp % 10;
-				tmp /= 10;
-			}if(sum == n) {
+		int n = Integer.parseInt(num);
+		int search = num.length() * 10;
+
+		for (int i = n - search; i < n; i++) {
+			int test = i;
+			int sum = test;
+			while (test>0) {
+				sum += test%10;
+				test/=10;
+			}
+			if(sum == n) {
 				System.out.println(i);
 				return;
 			}
