628368117e44b81d4f828589d62815fda4ecfa9a
SeonYoo-Kim <tjdn9@naver.com>
2026-02-15 18:38:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 148 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,36 +1,42 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int[] talls = new int[9];
-	static boolean[] selected = new boolean[9];
-	static boolean done;
+
+	static boolean[] selected;
+	static int[] heights;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < talls.length; i++) {
-			talls[i] = sc.nextInt();
+
+		heights = new int[9];
+		selected = new boolean[9];
+		for (int i = 0; i < 9; i++) {
+			heights[i] = sc.nextInt();
 		}
-		Arrays.sort(talls);
+		Arrays.sort(heights);
+
 		subset(0, 0, 0);
-		
+
 	}
+
 	private static void subset(int idx, int cnt, int sum) {
-		if(cnt > 7) return;
-		if(! done && cnt == 7 && sum == 100) {
-			for (int i = 0; i < 9; i++)
-				if(selected[i])
-					System.out.println(talls[i]);
-			done = true;
-			return;
+		if (cnt == 7 && sum == 100) {
+			for (int i = 0; i < 9; i++) {
+				if (selected[i]) {
+					System.out.println(heights[i]);
+				}
+			}
+			System.exit(0);
 		}
-		if(idx >= 9) return;
-		
+		if (idx >= 9 || cnt > 7 || sum > 100)
+			return;
+
 		selected[idx] = true;
-		subset(idx+1, cnt+1, sum + talls[idx]);
+		subset(idx + 1, cnt + 1, sum + heights[idx]);
 		selected[idx] = false;
-		subset(idx+1, cnt, sum);
-		
+		subset(idx + 1, cnt, sum);
+
 	}
 
 }
