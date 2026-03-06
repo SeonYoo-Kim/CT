7b7a400ea8dfb276a3c8565257a7574cafc936fd
SeonYoo-Kim <tjdn9@naver.com>
2026-03-06 10:22:30 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 116 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,9 +1,8 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -14,47 +13,54 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
-		int[][] freind = new int[N+1][N+1];
+
+		ArrayList<Integer>[] friends = new ArrayList[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			friends[i] = new ArrayList<>();
+		}
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			freind[a][b] = 1;
-			freind[b][a] = 1;
+			friends[a].add(b);
+			friends[b].add(a);
 		}
-//		int[] levels = new int[N+1];
+		
 		int min = Integer.MAX_VALUE;
-		int winner = 0;
+		int person = 0;
+		
 		for (int i = N; i > 0; i--) {
-			boolean[] selected = new boolean[N+1];
-			int levSum = 0;
+//			System.out.print(i + " ");
+			int KevinBacon = 0;
+			boolean[] visited = new boolean[N + 1];
 			Queue<int[]> q = new ArrayDeque<>();
-			int lev = 0;
-			q.offer(new int[] {i, lev});
-			selected[i] = true;
+			q.offer(new int[] {i, 0});
+			visited[i] = true;
 			
 			while(!q.isEmpty()) {
+				
 				int[] cur = q.poll();
 				int idx = cur[0];
-				lev = cur[1];
-				levSum += lev;
-// 				System.out.println(i + " levSum : " + levSum);
-				for(int j = N; j > 0; j--) {
-					if(!selected[j] && (freind[idx][j] != 0 || freind[j][idx] != 0)) {
-						q.offer(new int[] {j, lev+1});
-//						System.out.println("idx : " + idx + ", j : " +j+ ", lev+1 : " + (lev+1));
-						selected[j] = true;
+				int KB = cur[1];
+				KevinBacon += KB;
+				
+				for(int f : friends[idx]) {
+					
+					if(!visited[f]) {
+						visited[f] = true;
+						q.offer(new int[] {f, KB+1});
 					}
 				}
 			}
-			if(levSum <= min) {
-				min = levSum;
-				winner = i;
+//			System.out.println(KevinBacon);
+			if(KevinBacon <= min) {
+				min = KevinBacon;
+				person = i;
 			}
 		}
-		
-		System.out.println(winner);
+		System.out.println(person);
 	}
 
 }
