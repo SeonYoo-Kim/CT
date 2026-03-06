b906b38e62c4c00e2e99d34e23e92537cc25ccf2
SeonYoo-Kim <tjdn9@naver.com>
2026-03-06 14:12:21 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,93 +1,62 @@
-
-
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Queue;
 
 public class Main {
-	static List<Integer> list = new ArrayList<>();
-	static int[] dx = {0, 0, 1, -1};
-	static int[] dy = {-1, 1, 0, 0};
-	static boolean visited[][];
-	static int map[][], N;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		visited = new boolean[N][N];
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		boolean[][] visited = new boolean[N][N];
 		for (int i = 0; i < N; i++) {
-			String[] tmp = br.readLine().split("");
+			String line = br.readLine();
 			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(tmp[j]);
-			}
-		}//for(int[]a:map)System.out.println(Arrays.toString(a));
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(map[i][j] != 0)
-// 				dfs(i, j, 0);
-				bfs(i, j, 0);
+				map[i][j] = line.charAt(j) - '0';
 			}
 		}
-		
-		Integer[] arr = list.toArray(new Integer[0]);
-		Arrays.sort(arr);
-		System.out.println(arr.length);
-		for(int a : arr)
-			System.out.println(a);
-		
-	}
 
-	private static void bfs(int i, int j, int cnt) {
-		if(visited[i][j]) return;
-		
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {i, j});
-		visited[i][j] = true;
-		
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			cnt++;
-			int x = cur[0];
-			int y = cur[1];
-			
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dx[d];
-				int ny = y + dy[d];
-				
-				if(inRange(nx, ny)) {
-					q.offer(new int[] {nx, ny});
-					visited[nx][ny] = true;
+		int[] dx = { 0, 0, 1, -1 };
+		int[] dy = { 1, -1, 0, 0 };
+		ArrayList<Integer> apart = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+
+				if (!visited[i][j] && map[i][j] == 1) {
+					Queue<int[]> q = new ArrayDeque<>();
+					q.offer(new int[] { i, j });
+					visited[i][j] = true;
+					int cnt = 0;
+					while (!q.isEmpty()) {
+						int[] cur = q.poll();
+						cnt++;
+						int x = cur[0];
+						int y = cur[1];
+						for (int d = 0; d < 4; d++) {
+							int nx = x + dx[d];
+							int ny = y + dy[d];
+							if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] == 1) {
+								visited[nx][ny] = true;
+								q.offer(new int[] { nx, ny });
+							}
+						}
+					}
+					apart.add(cnt);
 				}
+
 			}
 		}
-		list.add(cnt);
-	}
-
-	private static void dfs(int x, int y, int cnt) {
-		
-		if(visited[x][y] || map[x][y] == 0) return;
-		
-		visited[x][y] = true;
-		
-		for (int d = 0; d < 4; d++) {
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			if(inRange(nx, ny)) {
-				dfs(nx, ny, cnt+1);
-				return;
-			}
-		}
-		list.add(cnt);
-	}
-
-	private static boolean inRange(int x, int y) {
-		return x >= 0 && x < N && y >= 0 && y < N && !visited[x][y] && map[x][y] !=0;
+		Integer[] list = apart.toArray(new Integer[0]);
+		Arrays.sort(list);
+		System.out.println(list.length);
+		for (int a : list)
+			System.out.println(a);
 	}
 
 }
