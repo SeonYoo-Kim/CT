b9bf813cd65ff718268ab60d18152b5a92a57853
SeonYoo-Kim <tjdn9@naver.com>
2026-03-06 10:36:49 +0900

    [Silver II] Title: 촌수계산, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,11 +1,9 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -17,40 +15,46 @@ public class Main{
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
+
 		int M = Integer.parseInt(br.readLine());
-		
-		ArrayList<Integer>[] edges = new ArrayList[N+1];
-		for (int i = 1; i <= N; i++) {
-			edges[i] = new ArrayList<>();
+
+		ArrayList<Integer>[] nodes = new ArrayList[N + 1];
+
+		for (int i = 1; i < N + 1; i++) {
+			nodes[i] = new ArrayList<>();
 		}
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int c = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
-			edges[c].add(d);
-			edges[d].add(c);
+			nodes[c].add(d);
+			nodes[d].add(c);
 		}
-		
-		Queue<Integer> q = new ArrayDeque<>();
-		int[] dist = new int[N+1];
-		Arrays.fill(dist, -1);
-		q.offer(a);
-		dist[a] = 0;
-		
-		while(!q.isEmpty()) {
+
+		Queue<int[]> q = new ArrayDeque<>();
+		boolean[] visited = new boolean[N + 1];
+		q.offer(new int[] { a, 0 });
+		visited[a] = true;
+
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int idx = cur[0];
+			int step = cur[1];
 			
-			int cur = q.poll();
-			if(cur == b) break;
-			
-			for(int child : edges[cur]) {
-				if(dist[child] == -1) {
-					dist[child] = dist[cur] + 1;
-					q.offer(child);
+			if(idx == b) {
+				System.out.println(step);
+				return;
+			}
+
+			for (int p : nodes[idx]) {
+
+				if (!visited[p]) {
+					visited[p] = true;
+					q.offer(new int[] { p, step + 1 });
 				}
 			}
-			
 		}
-		System.out.println(dist[b]);
+		System.out.println(-1);
 	}
 
 }
