94fe334776592a2b03555767bcae6b158cbad6eb
SeonYoo-Kim <tjdn9@naver.com>
2026-03-17 10:39:46 +0900

    [D3] Title: 두 수의 덧셈, Time: 94 ms, Memory: 27,776 KB -BaekjoonHub

---
+++ SWEA/D3/3260. 두 수의 덧셈/README.md
@@ -0,0 +1,15 @@
+# [D3] 두 수의 덧셈 - 3260 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBC1lOad9IDFAWr) 
+
+### 성능 요약
+
+메모리: 27,776 KB, 시간: 94 ms, 코드길이: 646 Bytes
+
+### 제출 일자
+
+2026-03-17 10:39
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/3260. 두 수의 덧셈/두 수의 덧셈.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			BigInteger a = new BigInteger(st.nextToken());
+			BigInteger b = new BigInteger(st.nextToken());
+			
+			System.out.println("#" + tc + " " + a.add(b));
+		}
+
+	}
+
+}
