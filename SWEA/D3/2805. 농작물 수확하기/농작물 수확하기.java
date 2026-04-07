fe9d72aaf37f30c82dc46221ab3deed779fb1dd6
SeonYoo-Kim <tjdn9@naver.com>
2026-04-07 11:08:38 +0900

    [D3] Title: 농작물 수확하기, Time: 93 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,40 +1,41 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int T = Integer.parseInt(br.readLine());
-
 		for (int tc = 1; tc <= T; tc++) {
-
 			int N = Integer.parseInt(br.readLine());
-			int n = N / 2;
-			int sum = 0;
-
+			int[][] map = new int[N][N];
+			
 			for (int i = 0; i < N; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				char[] arr = st.nextToken().toCharArray();
-//				System.out.println(Arrays.toString(arr));
+				char[] line = br.readLine().toCharArray();
 				for (int j = 0; j < N; j++) {
-					if (!(i + j < n || i + j > 3*n || i - j < (-1*n) || i - j > n)) {
-						/*
-						 * x+y가 2 미만이나 6 초과 x-y+4가 2 미만이나 6 초과
-						 */
-
-						sum += arr[j] - '0';
-						// System.out.print(arr[j] - '0' + " ");
-					}
-
-				} // System.out.println();
+					map[i][j] = line[j] - '0'; 
+				}
 			}
+			//for(int[] a:map)System.out.println(Arrays.toString(a));
+			
+			int sum = 0;
+			for (int i = 0; i < N; i++)
+				sum += map[N/2][i];
 
+			for (int i = 0; i < N/2; i++) {
+				for (int j = (N/2) - i; j <= (N/2) + i ; j++) {
+					sum += map[i][j];
+					sum += map[N - 1 -i][j];
+//					System.out.println("[" + i + ", " + j + "] : " + map[i][j]);
+//					System.out.println("[" + (N - 1 - i) + ", " + j + "] : " + map[N - 1 -i][j]);
+//					System.out.println(i + " : " + sum);
+				}
+			}
+			
+			
 			System.out.println("#" + tc + " " + sum);
 		}
 
