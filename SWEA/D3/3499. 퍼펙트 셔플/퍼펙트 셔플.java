fe0eb7d7f5cb5234445866c9f0c4aa4c64fe080c
SeonYoo-Kim <tjdn9@naver.com>
2026-04-07 10:49:24 +0900

    [D3] Title: 퍼펙트 셔플, Time: 106 ms, Memory: 31,104 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -2,27 +2,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine()) + 1;
-			String[] deque = br.readLine().split(" ");
+			
+			int N = Integer.parseInt(br.readLine());
+			String[] deque = new String[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) 
+				deque[i] = st.nextToken();
 			StringBuilder sb = new StringBuilder();
 			sb.append("#").append(tc).append(" ");
-			for (int i = 0; i < N/2; i++) {
-				sb.append(deque[i]).append(" ");
-				try {
-					sb.append(deque[i+N/2]).append(" ");
-				} catch (Exception e) {
-					break;
-				}
+			
+			int half = N/2;
+			if(N%2 != 0) half++;
+			int cnt = 0;
+			while(cnt < half) {
+				sb.append(deque[cnt++]).append(" ");
+				if(cnt+half-1 == N) break;
+				sb.append(deque[cnt+half-1]).append(" ");
 			}
+			
 			System.out.println(sb);
 		}
+
 	}
+
 }
