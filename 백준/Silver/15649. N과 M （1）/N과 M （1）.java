7a086c3885ec2372a5861a8fe549ada28f8d3fdb
SeonYoo-Kim <tjdn9@naver.com>
2026-04-08 11:36:32 +0900

    [Silver III] Title: N과 M (1), Time: 292 ms, Memory: 26168 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,50 +1,40 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main {
+public class Main{
 
-	private static int M;
-	private static int N;
-	private static int[] selected;
-	private static StringBuilder sb = new StringBuilder();
-	private static boolean[] visited;
+	static boolean selected[];
+	static int M, N, arr[];
+	static List<int[]> list;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		visited = new boolean[N + 1];
-		selected = new int[M]; 
-
-		def(0);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		selected = new boolean[N];
+		arr = new int[M];
+		comb(0, sb);
 
 		System.out.println(sb);
-		
-
 	}
 
-	private static void def(int depth) {
-		if (depth == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(selected[i]).append(" ");
-			}
+	private static void comb(int cnt, StringBuilder sb) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++)
+				sb.append(arr[i]).append(" ");
 			sb.append("\n");
 			return;
 		}
-		for (int i = 1; i <= N; i++) {
-			if (visited[i])
-				continue;
-			visited[i] = true;
-			selected[depth] = i;
-			def(depth + 1);
-			visited[i] = false;
+
+		for (int i = 0; i < N; i++) {
+			if (!selected[i]) {
+				selected[i] = true;
+				arr[cnt] = i + 1;
+				comb(cnt + 1, sb);
+				selected[i] = false;
+			}
 		}
 	}
-
 }
