673e8971a1eb5acd7802a7052e76f4130ed03f62
SeonYoo-Kim <tjdn9@naver.com>
2026-04-08 09:35:51 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 79 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,69 +1,58 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Solution {
-
-	static int map[][];
-	static int[] dx = { 0, 0, 1, -1 };
-	static int[] dy = { 1, -1, 0, 0 };
+	
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
+	static int[][] map;
 	static boolean[][] visited;
-	static int asw;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for (int tc = 1; tc <= 10; tc++) {
-			asw = 0;
 			br.readLine();
-
 			map = new int[16][16];
 			visited = new boolean[16][16];
-			int x = -1, y = -1;
-
+			int curX = -1, curY = -1;
 			for (int i = 0; i < 16; i++) {
-				String line = br.readLine();
+				char[] line = br.readLine().toCharArray();
 				for (int j = 0; j < 16; j++) {
-					map[i][j] = line.charAt(j) - '0';
-					if (map[i][j] == 2) {
-						x = i;
-						y = j;
+					map[i][j] = line[j] - '0';
+					if(map[i][j] == 2) {
+						curX = i;
+						curY = j;
+						visited[i][j] = true;
 					}
 				}
 			}
-//for(int[]a:map)System.out.println(Arrays.toString(a));
-			
-			dfs(x, y);
-			
-			System.out.println("#" + tc + " " + asw);
+						
+			System.out.println("#" + tc + " " + dfs(curX, curY));
 		}
 
 	}
 
-	private static void dfs(int x, int y) {
-//		System.out.println("x, y : " + x + " " + y);
-		if(asw == 1) return;
+	private static int dfs(int x, int y) {
 		
-		if (map[x][y] == 3) {
-			asw = 1;
-			return;
+		if(map[x][y] == 3) {
+			return 1;
 		}
-		
-		visited[x][y] = true;
-		
 		for (int d = 0; d < 4; d++) {
+			
 			int nx = x + dx[d];
 			int ny = y + dy[d];
-
-			if (inRange(nx, ny)) {
-				dfs(nx, ny);
+			
+			if(inRange(nx, ny)) {
+				visited[nx][ny] = true;
+				if (dfs(nx, ny) == 1) return 1;
 			}
 		}
+		return 0;
 	}
 
 	private static boolean inRange(int x, int y) {
-		return x >= 0 && x < 16 && y >= 0 && y < 16 && map[x][y] != 1 && !visited[x][y];
+		return x >= 0 && x < 16 && y >= 0 && y < 16 && !visited[x][y] && map[x][y]!=1;
 	}
 
 }
