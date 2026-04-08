434e57270485e9fdbc90a187e70e9b46e8f97c52
SeonYoo-Kim <tjdn9@naver.com>
2026-04-08 10:04:25 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 94 ms, Memory: 29,824 KB -BaekjoonHub

--- SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -1,69 +1,54 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Solution {
 
-	static int map[][];
-	static int[] dx = { 0, 0, 1, -1 };
-	static int[] dy = { 1, -1, 0, 0 };
-	static boolean visited[][];
-
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {0, 0, 1, -1};
+	static int[] dy = {1, -1, 0, 0};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		for (int tc = 1; tc <= 10; tc++) {
-
-			int testCase = Integer.parseInt(br.readLine());
+			br.readLine();
 			map = new int[100][100];
 			visited = new boolean[100][100];
-			int x = -1;
-			int y = -1;
-
+			int curX = -1, curY = -1;
 			for (int i = 0; i < 100; i++) {
-				String line = br.readLine();
-				String[] arr = line.split("");
+				char[] line = br.readLine().toCharArray();
 				for (int j = 0; j < 100; j++) {
-					map[i][j] = Integer.parseInt(arr[j]);
-					if (map[i][j] == 2) {
-						x = i;
-						y = j;
+					map[i][j] = line[j] - '0';
+					if(map[i][j] == 2) {
+						curX = i;
+						curY = j;
+						visited[i][j] = true;
 					}
 				}
 			}
-//			for(int[]a : map)
-//				System.out.println(Arrays.toString(a));
-
-			System.out.println("#" + tc + " " + maze(x, y));
+			System.out.println("#" + tc + " " + dfs(curX, curY));
 		}
 
 	}
 
-	private static int maze(int x, int y) {
-		// System.out.println(x + " " + y + " " + map[x][y]);
-		
-		if(map[x][y] == 3) {
+	private static int dfs(int x, int y) {
+		if(map[x][y] == 3)
 			return 1;
-		}
-		
-		if(visited[x][y]) {
-			System.out.println("visited["+x+"]["+y+"] : " + visited[x][y]);
-			return 0;
-		}
-		visited[x][y] = true;
-		
 		for (int d = 0; d < 4; d++) {
 			int nx = x + dx[d];
 			int ny = y + dy[d];
-			
-			if(nx>=0 && nx <100 && ny>=0 && ny <100 && map[nx][ny] != 1 && !visited[nx][ny]) {
-				if(maze(nx, ny) == 1) {
-	                return 1;
-	            }
+			if(inRange(nx, ny)) {
+				visited[nx][ny] = true;
+				if(dfs(nx, ny) == 1) return 1;
 			}
 		}
 		return 0;
+	}
+
+	private static boolean inRange(int x, int y) {
+		return x >= 0 && x < 100 && y >= 0 && y < 100 && !visited[x][y] && map[x][y]!=1;
 	}
 
 }
