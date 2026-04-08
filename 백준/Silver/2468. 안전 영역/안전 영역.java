7ca0f2b7a31bc338714568c803232b4cbaeb6cb8
SeonYoo-Kim <tjdn9@naver.com>
2026-04-08 10:59:06 +0900

    [Silver I] Title: 안전 영역, Time: 340 ms, Memory: 41644 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,74 +2,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main{
-	static boolean[][] visited;
-	static boolean[][] drowned;
-	static int[] dx = new int[] { 0, 0, 1, -1 };
-	static int[] dy = new int[] { 1, -1, 0, 0 };
-	static int N;
+	static int map[][], N;
+	static boolean visited[][];
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
 		N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N][N];
-
-		int max_space = 0;
-		int max_height = 0;
-
+		map = new int[N][N];
+		int heighest = 0;
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				max_height = Math.max(max_height, arr[i][j]);
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] > heighest)
+					heighest = map[i][j];
 			}
+
 		}
-		for (int rain = 0; rain <= max_height; rain++) {
-			drowned = new boolean[N][N]; // visited랑 같이 가야해서 계속 초기화 필요
+		int max = 0;
+		int[] dx = { 0, 0, 1, -1 };
+		int[] dy = { 1, -1, 0, 0 };
+
+		for (int t = 0; t <= heighest; t++) {
+
+			ArrayDeque<int[]> q = new ArrayDeque<>();
 			visited = new boolean[N][N];
-			int space = 0;
+			int cnt = 0;
+
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if (arr[i][j] <= rain) {
-						drowned[i][j] = true;
+
+					if (!visited[i][j] && map[i][j] > t) {
+						visited[i][j] = true;
+						q.offer(new int[] { i, j });
+
+						while (!q.isEmpty()) {
+
+							int[] cur = q.poll();
+							int x = cur[0];
+							int y = cur[1];
+
+							for (int d = 0; d < 4; d++) {
+								int nx = x + dx[d];
+								int ny = y + dy[d];
+
+								if (inRange(nx, ny, t)) {
+									visited[nx][ny] = true;
+									q.offer(new int[] { nx, ny });
+								}
+							}
+						}
+						cnt++;
 					}
 				}
 			}
-			// 인접 영역 탐색
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (!drowned[i][j] && !visited[i][j]) { /////////////
-						dfs(i, j); /////////////////// 안전구역이 끝나야 리턴됨
-						space++;
-					}
-				}
-			}
-			if (space > max_space) {
-				max_space = space;
-			}
+			if (cnt > max)
+				max = cnt;
 		}
 
-		System.out.println(max_space);
+		System.out.println(max);
 	}
 
-	private static void dfs(int x, int y) {
-
-		visited[x][y] = true;
-
-		for (int d = 0; d < 4; d++) {
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-
-			if (nx < 0 || ny < 0 || nx >= N || ny >= N || drowned[nx][ny] || visited[nx][ny])
-				continue; // 안전구역 인접이 전부 이래야 끝남
-
-			dfs(nx, ny);
-
-		}
+	private static boolean inRange(int x, int y, int t) {
+		return x >= 0 && x < N && y >= 0 && y < N && !visited[x][y] && map[x][y] > t;
 	}
 }
