4e4fcf98000c79bcadf713e3614bbafaf8c860bb
SeonYoo-Kim <tjdn9@naver.com>
2026-04-09 14:44:58 +0900

    [Gold V] Title: 탑, Time: 732 ms, Memory: 90632 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,30 +1,33 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		Stack<int[]> tower = new Stack<>();		
-		
+		int[] heights = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			heights[i] = Integer.parseInt(st.nextToken());
+		
+		Stack<int[]> receive = new Stack<>();
 		StringBuilder sb = new StringBuilder();
-		int max = 0;
+		
 		for (int i = 0; i < N; i++) {
-			int height = Integer.parseInt(st.nextToken());
 			
-			while(!tower.isEmpty() && tower.peek()[1] < height) 
-				tower.pop();
+			while(!receive.isEmpty() && heights[i] >= receive.peek()[1])
+				receive.pop();
 			
+			if(receive.isEmpty()) sb.append("0 ");
+			else sb.append(receive.peek()[0]).append(" ");
 			
-			if(tower.isEmpty()) sb.append("0 ");
-			else sb.append(tower.peek()[0]).append(" ");
-			
-			tower.push(new int[] {i + 1, height});
+			receive.add(new int[] {i + 1, heights[i]});
 			
 		}
 		System.out.println(sb);
