e9c855a7918690d9fd23b4846c8dbcf61c979301
SeonYoo-Kim <tjdn9@naver.com>
2026-04-09 14:09:16 +0900

    [Silver IV] Title: 수들의 합 2, Time: 120 ms, Memory: 14952 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,30 +1,37 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[Integer.parseInt(st.nextToken())];
+		
+		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < arr.length; i++)
+		for (int i = 0; i < N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
-		int sum = 0, cnt = 0;
-		int s = 0, e = 0;
+		
+		int s = 0, e = 0, cnt = 0, sum = 0;
+		
 		while(true) {
-			if(sum <= M) {
-				if(sum == M)
-					cnt++;
-				if(e == arr.length) break;
-				sum += arr[e++];
-			}else
-				sum -= arr[s++];
 			
+			if(sum < M) {
+				if(e == N) break;
+				sum += arr[e++];
+				
+			}else {
+				if(sum == M) cnt++;
+				sum -= arr[s++];
+			}
 		}
+		
 		System.out.println(cnt);
 	}
+
 }
