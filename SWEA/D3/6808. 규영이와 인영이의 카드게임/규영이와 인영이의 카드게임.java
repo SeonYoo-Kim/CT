fa0dffc64420c8b4fcd76d04d6d9dd465addcdab
SeonYoo-Kim <tjdn9@naver.com>
2026-04-14 16:43:47 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,686 ms, Memory: 33,152 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -5,77 +5,54 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int winCnt, loseCnt, N = 9;
-	static int[] kyu, in;
-	static boolean[] selected;
 
+	private static int[] cardsK, cardsI, set;
+	static int winK, winI;
+	static boolean[] selected;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int T = Integer.parseInt(br.readLine());
-
 		for (int tc = 1; tc <= T; tc++) {
-			winCnt = 0;
-			loseCnt = 0;
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			kyu = new int[N];
-			in = new int[N];
-			selected = new boolean[N * 2 + 1];
-			for (int i = 0; i < N; i++) {
-				kyu[i] = Integer.parseInt(st.nextToken());
-				selected[kyu[i]] = true;
-			}
+			cardsK = new int[9];
+			cardsI = new int[9];
+			set = new int[9];
+			winK = winI = 0;
+			selected = new boolean[9];
+			for (int i = 0; i < 9; i++)
+				cardsK[i] = Integer.parseInt(st.nextToken());
 			int idx = 0;
-			for (int i = 1; i <= N * 2; i++) {
-				if (!selected[i])
-					in[idx++] = i;
+			for (int i = 1; i <= 18; i++) {
+				int tmp = i;
+				if (Arrays.stream(cardsK).noneMatch(x -> x == tmp))
+					cardsI[idx++] = i;
 			}
-//			System.out.println("규영 : "+Arrays.toString(kyu));
-//			System.out.println("인영 : "+Arrays.toString(in));
-			Arrays.sort(in);
-			do {
-				int kScore = 0, iScore = 0;
-				for (int i = 0; i < N; i++) {
-					if (kyu[i] > in[i])
-						kScore += kyu[i] + in[i];
-					else
-						iScore += kyu[i] + in[i];
-				}
-				if (kScore > iScore)
-					winCnt++;
-				else
-					loseCnt++;
-			} while (np());
-
-			System.out.println("#" + tc + " " + winCnt + " " + loseCnt);
+			dfs(0, 0);
+			System.out.println("#" + tc + " " + winK + " " + winI);
 		}
-
 	}
 
-	private static boolean np() {
-		// System.out.println("in next permutation");
-
-		int i = N - 1;
-		while (i >= 1 && in[i - 1] > in[i]) i--;
-
-		if (i == 0) return false;
-
-		int j = N - 1;
-		while (in[i - 1] > in[j]) j--;
-		//System.out.println(i + " " + j);
-		swap(i - 1, j);
-
-		int k = N - 1;
-		while (i < k) swap(i++, k--);
-
-		return true;
+	private static void dfs(int turn, int idx) {
+		if(turn == 9) {
+			int scoreK = 0;
+			int scoreI = 0;
+			for (int i = 0; i < 9; i++) {
+				int score = cardsK[i] + set[i];
+				if(cardsK[i] > set[i]) scoreK += score; 
+				else scoreI += score;
+			}
+			if(scoreK > scoreI) winK++;
+			else winI++;
+			return;
+		}
+		
+		for (int i = 0; i < 9; i++) {
+			if(selected[i]) continue;
+			
+			selected[i] = true;
+			set[idx] = cardsI[i];
+			dfs(turn+1, idx + 1);
+			selected[i] = false;
+		}
 	}
-
-	private static void swap(int i, int j) {
-		int temp = in[i];
-		in[i] = in[j];
-		in[j] = temp;
-	}
-
 }
