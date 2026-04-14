e38995a0bec410b8123eafd1aa167ecf67bbe84b
SeonYoo-Kim <tjdn9@naver.com>
2026-04-14 17:56:38 +0900

    [D4] Title: 정사각형 방, Time: 331 ms, Memory: 34,768 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -2,66 +2,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StreamTokenizer;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Solution{
+public class Solution {
 
-	static int[][] room;
+	static int N, room[][], max, argMax;
 	static int[] dx = { 0, 0, 1, -1 };
 	static int[] dy = { 1, -1, 0, 0 };
-	static int N, max, value;
 
-	public static void main(String[] args) throws IOException {
-		StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-		st.nextToken();
-		int T = (int) st.nval;
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			room = new int[N][N];
 
-			st.nextToken();
-			N = (int) st.nval; // 1000 이하
-			room = new int[N][N]; // 1 이상 n^2 이하 중복없음
-			// 상하좌우로 이동 가능한데 현재방보다 1 커야만 갈수있음
 			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					st.nextToken();
-					room[i][j] = (int) st.nval;
-				}
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++)
+					room[i][j] = Integer.parseInt(st.nextToken());
 			}
-
 			max = 0;
-			value = 0;
-			for (int i = 0; i <N; i++) {
-				for (int j = 0; j <N; j++) {
-					int len = move(i, j);
-					
-					if(len > max) {
+			argMax = N * N;
+			for (int i = N - 1; i >= 0; i--) {
+				for (int j = N - 1; j >= 0; j--) {
+					int len = dfs(i, j);
+					if (len > max) {
 						max = len;
-						value = room[i][j];
-					}else if(len == max && room[i][j]<value) {
-						value = room[i][j];
+						argMax = room[i][j];
+					}else if(len == max) {
+						argMax = Math.min(room[i][j], argMax);
 					}
 				}
 			}
-			System.out.println("#" + tc + " " + value + " " + max);
+			System.out.println("#" + tc + " " + argMax + " " + max);
 		}
 	}
 
-	private static int move(int x, int y) {
-		int cnt = 1;
-
+	private static int dfs(int x, int y) {
+		int len = 1;
 		for (int d = 0; d < 4; d++) {
 			int nx = x + dx[d];
 			int ny = y + dy[d];
 
-			if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-
-				if (room[x][y] + 1 == room[nx][ny]) {
-					return cnt + move(nx, ny);
-				}
-			}
+			if (inRange(nx, ny) && room[x][y] + 1 == room[nx][ny])
+				len = 1 + dfs(nx, ny);
 		}
-		return cnt;
+		return len;
 	}
+
+	private static boolean inRange(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < N;
+	}
+
 }
