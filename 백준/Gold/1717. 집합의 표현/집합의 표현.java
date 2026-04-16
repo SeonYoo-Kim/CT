fd388d2bfe7e4e784f03cc1a69d23b1fb6635fe7
SeonYoo-Kim <tjdn9@naver.com>
2026-04-16 11:35:44 +0900

    [Gold V] Title: 집합의 표현, Time: 956 ms, Memory: 52160 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,13 +1,13 @@
-
+ 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Main{
 
-	static int[] parents;
+	private static int[] parents;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,9 +20,9 @@ public class Main{
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-			if(op == 0) { // a 합 b
+			if(op == 0) {
 				union(a, b);
-			}else { // a == b ??
+			}else {
 				System.out.println(find(a) == find(b) ? "YES" : "NO");
 			}
 		}
@@ -31,21 +31,19 @@ public class Main{
 	private static void union(int a, int b) {
 		int rootA = find(a);
 		int rootB = find(b);
-		if(rootA != rootB) {
+		if(rootA != rootB)
 			parents[rootB] = rootA;
-		}
 	}
 
-	private static int find(int n) {
-		if(parents[n] == n) return n;
-		return parents[n] = find(parents[n]);
+	private static int find(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = find(parents[a]);
 	}
 
 	private static void makeSet(int n) {
-		parents = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
+		parents = new int[n+1];
+		for (int i = 1; i <= n; i++)
 			parents[i] = i;
-		}
 	}
 
 }
