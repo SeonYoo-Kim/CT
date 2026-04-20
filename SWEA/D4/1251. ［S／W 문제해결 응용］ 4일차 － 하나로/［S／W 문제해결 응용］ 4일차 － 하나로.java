0954273241eeef46de9902bff16b0636cc5381ee
SeonYoo-Kim <tjdn9@naver.com>
2026-04-20 15:38:11 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 618 ms, Memory: 99,332 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -3,75 +3,85 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static class Island{
-		int to;
+	static class edge{
+		int from, to;
 		long weight;
 		
-		public Island(int to, long weight) {
+		public edge(int from, int to, long weight) {
 			super();
+			this.from = from;
 			this.to = to;
 			this.weight = weight;
 		}
 	}
-	
+	static int[] parents;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
+			parents = new int[N];
+			makeSet(N);
+			
 			int[] x = new int[N];
 			int[] y = new int[N];
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++)
 				x[i] = Integer.parseInt(st.nextToken());
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++)
 				y[i] = Integer.parseInt(st.nextToken());
-			
 			double E = Double.parseDouble(br.readLine());
 			
-			ArrayList<Island>[] graph = new ArrayList[N+1];
-			for (int i = 0; i <= N; i++)
-				graph[i] = new ArrayList<>();
-			
+			ArrayList<edge> graph = new ArrayList<>();
 			for (int i = 0; i < N; i++) {
-				for (int j = i + 1; j < N; j++) {
+				for (int j = i; j < N; j++) {
 					long dx = x[i] - x[j];
 					long dy = y[i] - y[j];
-					
-					long weight = dx*dx + dy*dy; 
-					graph[i].add(new Island(j, weight));
-					graph[j].add(new Island(i, weight));
+					long weight = dx * dx + dy * dy; 
+					graph.add(new edge(i, j, weight));
 				}
 			}
-
-			PriorityQueue<Island> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o1.weight, o2.weight));
-			pq.offer(new Island(0, 0));
-			boolean[] visited = new boolean[N + 1];
-			long tax = 0;
-			int cnt = 0;
+			graph.sort((a, b) -> Long.compare(a.weight, b.weight));
 			
-			while(!pq.isEmpty()) {
-				Island cur = pq.poll();
+			int cnt = 0;
+			long sum = 0;
+			for (int i = 0; i < graph.size(); i++) {
 				
-				if(visited[cur.to]) continue;
-				visited[cur.to] = true;
+				if(Union(graph.get(i).from, graph.get(i).to)) {
+					cnt++;
+					sum += graph.get(i).weight;
+				}
+					
+				if(cnt == N - 1) break;
 				
-				tax += cur.weight;
-				cnt++;
-				
-				if(cnt == N) break;
-				
-				for (Island next : graph[cur.to])
-					if(!visited[next.to])
-						pq.offer(next);
 			}
-			System.out.println("#" + tc + " " + Math.round(tax * E));
+			
+			
+			
+			System.out.println("#" + tc + " " + Math.round(sum * E));
 		}
 	}
+	private static boolean Union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+		if(rootA == rootB) return false;
+		parents[rootB] = rootA;
+		return true;
+	}
+	private static int find(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = find(parents[a]);
+	}
+	private static void makeSet(int n) {
+		for (int i = 0; i < n; i++)
+			parents[i] = i;		
+	}
+
 }
