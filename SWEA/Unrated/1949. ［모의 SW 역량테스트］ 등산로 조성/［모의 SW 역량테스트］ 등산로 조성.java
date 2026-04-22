89fa8014b798e4ebebf3abe4910c2ed70d07fe46
SeonYoo-Kim <tjdn9@naver.com>
2026-04-22 10:00:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 84 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,15 +1,16 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class Solution{
 
-	static int N, K, map[][], tops[][], max;
-	static boolean[][] visited;
+	static int N, K, map[][], tops[][], len, asw;
 	static int[] dx = { 0, 0, 1, -1 };
 	static int[] dy = { 1, -1, 0, 0 };
+	static boolean[][] visited;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,77 +24,71 @@ public class Solution {
 			tops = new int[5][2];
 			for (int[] top : tops)
 				Arrays.fill(top, -1);
-
-			int tall = 0;
+			int max = 0;
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					if (map[i][j] > tall)
-						tall = map[i][j];
+					if (map[i][j] > max)
+						max = map[i][j];
 				}
 			}
-			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (map[i][j] == tall) {
-						tops[cnt][0] = i;
-						tops[cnt++][1] = j;
+			int idx = 0;
+			for (int i = 0; i < N; i++)
+				for (int j = 0; j < N; j++)
+					if (map[i][j] == max) {
+						tops[idx][0] = i;
+						tops[idx++][1] = j;
 					}
-				}
-			}
-
-			max = 0;
+			asw = 0;
 			for (int[] top : tops) {
-				// len = 0;
 				int x = top[0];
 				int y = top[1];
-				if (x == -1 || y == -1)
-					break;
-				int[][] copy = new int[N][N];
-				for (int i = 0; i < N; i++) {
-					copy[i] = map[i].clone();
+
+				if (x != -1 && y != -1) {
+					len = -1;
+					visited = new boolean[N][N];
+					visited[x][y] = true;
+					dfs(x, y, 0, 1);
+					
 				}
-				visited = new boolean[N][N];
-				visited[x][y] = true;
-				dfs(x, y, 0, 1);
 
 			}
 
-			System.out.println("#" + tc + " " + max);
+			System.out.println("#" + tc + " " + asw);
 		}
 
 	}
 
 	private static void dfs(int x, int y, int broken, int len) {
 
-		max = Math.max(len, max);
+		if(len > asw) asw = len;
+		
 
 		for (int d = 0; d < 4; d++) {
 			int nx = x + dx[d];
 			int ny = y + dy[d];
-
-			if (!inRange(nx, ny) || visited[nx][ny]) continue;
-
-			if (map[nx][ny] < map[x][y]) {
-				visited[nx][ny] = true;
-				dfs(nx, ny, broken, len + 1);
-				visited[nx][ny] = false;
-			}
-			else {
-				if (broken == 0 && map[nx][ny] - K < map[x][y]) {
-					int original = map[nx][ny];
-					map[nx][ny] = map[x][y] - 1;
-					
-					visited[nx][ny] = true;
-					dfs(nx, ny, 1, len + 1);
-					visited[nx][ny] = false;
-					
-					map[nx][ny] = original;
-				}
+			if (inRange(nx, ny) && !visited[nx][ny]) {
 				
-			}
+				if (map[nx][ny] < map[x][y]) {
+					// System.out.println(nx + ", " + ny + " : " + map[nx][ny] + " -> not break & len : " + len);
+					visited[nx][ny] = true;
+					dfs(nx, ny, broken, len + 1);
+					visited[nx][ny] = false;
 
+				} else {
+					if (broken == 0 && map[nx][ny] - K < map[x][y]) {
+						// System.out.println(x + ", " + y + " -> " + (map[nx][ny] - K)  + ", " + map[x][y]);
+						int origin = map[nx][ny]; 
+						map[nx][ny] = map[x][y] - 1;
+						// System.out.println(nx + ", " + ny + " : " + map[nx][ny] + " breaked & -> len : " + len);
+						visited[nx][ny] = true;
+						dfs(nx, ny, 1, len + 1);
+						map[nx][ny] = origin;
+						visited[nx][ny] = false;
+					}
+				}
+			}
 		}
 	}
 
