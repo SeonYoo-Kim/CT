686dccf68de73a2d972e13e983b5f20bc0cf4c94
SeonYoo-Kim <tjdn9@naver.com>
2026-04-28 16:51:56 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT H.FLAVOR 
+from FIRST_HALF H
+join ICECREAM_INFO I on H.FLAVOR = I.FLAVOR
+where H.TOTAL_ORDER > 3000 && I.INGREDIENT_TYPE='fruit_based'
+order by H.TOTAL_ORDER desc;
