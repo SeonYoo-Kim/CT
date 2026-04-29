5ac6de2658af7b49fd5080531459ad64def9e9c5
SeonYoo-Kim <tjdn9@naver.com>
2026-04-29 13:37:15 +0900

    Refactor SQL query to find repeat purchases

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,10 +1,5 @@
--- 코드를 입력하세요
--- SELECT * from ONLINE_SALE ;
 SELECT distinct USER_ID, PRODUCT_ID
 from ONLINE_SALE 
-where (USER_ID, PRODUCT_ID) in(
-        select USER_ID, PRODUCT_ID from ONLINE_SALE 
-        group by USER_ID, PRODUCT_ID
-        having count(*) >= 2
-        )
+group by USER_ID, PRODUCT_ID
+having count(*) >= 2        
 order by USER_ID, PRODUCT_ID desc;
