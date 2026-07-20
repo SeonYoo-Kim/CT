1635ea59247aea7ed46f779e95d40c8570c28a75
SeonYoo-Kim <tjdn9@naver.com>
2026-07-21 06:51:51 +0900

    [Passed] 요일 맞추기 (96ms, 10MB)

---
+++ trail2/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+        
+        String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int day1 = d1, day2 = d2;
+        
+        for (int i = 0; i < m1; i++) 
+            day1 += months[i];
+        for (int i = 0; i < m2; i++) 
+            day2 += months[i];
+        
+        int idx = ((day2-day1) % 7 + 7) % 7;
+        System.out.println(days[idx]);
+
+    }
+}
