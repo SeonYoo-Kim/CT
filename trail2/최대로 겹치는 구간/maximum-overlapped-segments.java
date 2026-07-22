0bd5502a61b6887e0208ed4e4303a27a96153dbf
SeonYoo-Kim <tjdn9@naver.com>
2026-07-22 21:26:43 +0900

    [Passed] 최대로 겹치는 구간 (99ms, 9MB)

---
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[402];
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = (Integer.parseInt(st.nextToken()) + 100)* 2;
+            int e = (Integer.parseInt(st.nextToken()) + 100)* 2;
+            for (int j = s; j < e; j++) {
+                arr[j]++;
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(max < arr[i]) max = arr[i];
+        }
+        System.out.println(max);
+    }
+
+}
